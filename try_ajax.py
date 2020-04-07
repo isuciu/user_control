915418230acd765729dc2ff6d0d22a24f3194bbc
isuciu <isuciu0@uoc.edu>
2020-04-07 15:14:29 +0200

    buttons turning green on mqtt message sent

--- try_ajax.py
+++ try_ajax.py
@@ -10,17 +10,14 @@ counter = -1
 CORS(app)
 connection = psycopg2.connect(user="mainflux",
                                   password="mainflux",
-                                  host="172.18.0.6",
+                                  host="172.18.0.3",
                                   port="5432",
                                   database="things")
 cursor = connection.cursor()
 
 broker_address= "localhost"
 port = 1883
-clientID = "server"
-client = mqtt.Client(clientID)               #create new instance
-
-
+           #create new instance
 
 def query_db(publisher):
 	postgreSQL_select_Query = "select things.* from things things where things.id='"+ str(publisher)+ "'"
@@ -36,13 +33,12 @@ def query_db(publisher):
 		return thing_id,thing_key
 
 def on_connect(client, userdata, flags, rc):
-    if rc == 0:
-        print("Connected to broker")
-        global Connected                #Use global variable
-        Connected = True                #Signal connection 
-    else:
-        print("Connection failed")
-client.on_connect= on_connect                      #attach function to callback
+	print("inside on connect")
+	if rc == 0:
+		client.connected_flag=True               #Signal connection 
+	else:
+		client.connected_flag=False
+                   #attach function to callback
 
 @app.route('/tryingajax/SetSR/<publisher>/<channel>/sensors/<sensor_name>')
 def hello(publisher, channel, sensor_name):
@@ -54,18 +50,27 @@ def hello(publisher, channel, sensor_name):
 
 
 @app.route('/SendMessage/<selectedvalue>/<publisher>/<channel>/sensors/<sensor_name>')
-def square(selectedvalue, publisher, channel, sensor_name):
+def sendmessage(selectedvalue, publisher, channel, sensor_name):
 	thing_id, thing_key= query_db(publisher)
 	if (thing_id != 0 and thing_key != 0):
+		mqtt.Client.connected_flag=False
+		client = mqtt.Client()    
 		client.username_pw_set(thing_id, thing_key)    #set username and password
-		client.connect(broker_address, port=port)
-		topic= "channels/" + str(channel) +  "/control"
-		timestamp = time.time()
-		data = {"type": "SET_SR", "sensor":sensor_name, "v":selectedvalue, "u":"s", "t":timestamp}
-		client.publish(topic,json.dumps(data)) 
-		client.disconnect()
-	answer = "button press " + str(selectedvalue) + " sensor: " + sensor_name
-	return answer
+		client.on_connect=on_connect
+		try:
+			client.connect(broker_address, port=port)
+			client.loop_start()
+			topic= "channels/" + str(channel) +  "/control"
+			timestamp = time.time()
+			data = {"type": "SET_SR", "sensor":sensor_name, "v":selectedvalue, "u":"s", "t":timestamp}
+			client.publish(topic,json.dumps(data)) 
+			client.disconnect()
+			client.loop_stop()
+			return "OK"
+		except:
+			return "Connection to device failed, try again"
+	else:
+		return "Failed setting SR, try again"
 
 if __name__ == '__main__':
 	app.run()
