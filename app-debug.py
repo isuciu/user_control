13893c84449a1f36f2d7f296a06d0ab4d64ecf42
isuciu <isuciu0@uoc.edu>
2020-12-02 17:01:07 +0100

    user can edit the alarms in grafana dashboard

--- app-debug.py
+++ app-debug.py
@@ -5,6 +5,7 @@ import paho.mqtt.client as mqtt
 import time
 import json
 import os
+import grafana_interactions as gr
 
 app = Flask(__name__)
 CORS(app)
@@ -107,6 +108,141 @@ def cal_sensor(db_to_use,target_device,channel,sensorname):
 			return "Failed calibrating sensor, try again"	
 	except:
 		return "OK"	
+
+
+@app.route('/control/SetAlarm/<channel>/sensors/<sensorname>/<organization>/<dashboard_name>/<user_login>')
+def alarmpage(channel, sensorname, organization, dashboard_name, user_login):
+#TBD: USER_LOGIN--> USER_EMAIL. to check in channels table the existing link between channel and user
+	# for now: just check that channel is valid, or exists
+
+	if sensorname == "Temperature-S":
+		pan_order = 2 #panel order in Alertes dashboard
+	elif sensorname == "AirCO2":
+		pan_order = 3
+	elif sensorname == "WaterLevel":
+		pan_order = 4
+	elif sensorname == "Oxygen":
+		pan_order = 5
+	elif sensorname == "AtmosphericTemp":
+		pan_order = 6
+	elif sensorname == "Conductivity2":
+		pan_order = 7
+	elif sensorname == "Conductivity1":
+		pan_order = 8
+	elif sensorname == "Turbidity":
+		pan_order = 9
+	elif sensorname == "pH":
+		pan_order = 10
+	elif sensorname == "Humidity":
+		pan_order = 11
+	elif sensorname == "Temperature-D":
+		pan_order = 12
+	else:
+		message = "Invalid sensor name" #will appear in div
+		return render_template('alarm.html', message= message, sensor=sensorname, min_val="None", max_val = "None" , channel=channel,
+			 organization=organization, dashboard_name=dashboard_name, user_login=user_login)
+		
+	#try:
+	#	user_email = query_channels(channel)
+	#except:
+	#	message = "Db Query error"
+	#	return render_template('alarm.html', message= message, sensor=sensorname, min_val = "None", max_val= "None", channel=channel,
+	#		 organization=organization, dashboard_name=dashboard_name, user_login=user_login)
+	user_email = 1
+	if user_email is not None: #so channel exists
+		try:
+			#will extract the json definition of this particular dashboard
+			#this fc already switches organization
+			print("Obtaining dashboard json")  
+			data=gr._get_dashboard_json(dashboard_name, organization) #collapsed has to be True for all rows!
+			crt_min = data['dashboard']['panels'][pan_order]['panels'][0]['alert']['conditions'][0]['evaluator']['params'][0]
+			crt_max = data['dashboard']['panels'][pan_order]['panels'][0]['alert']['conditions'][0]['evaluator']['params'][1]
+
+			return render_template('alarm.html', message= "", sensor=sensorname, min_val=crt_min, max_val = crt_max, channel=channel,
+			 organization=organization, dashboard_name=dashboard_name, user_login=user_login)
+		except:
+			message = "Something went wrong when querying the alarm"
+			return render_template('alarm.html', message= message, sensor=sensorname, min_val="None", max_val = "None" , channel=channel,
+			 organization=organization, dashboard_name=dashboard_name, user_login=user_login)
+	else:
+		message = "Invalid channel"
+		return render_template('alarm.html', message= message, sensor=sensorname, min_val="None", max_val = "None", channel=channel,
+			 organization=organization, dashboard_name=dashboard_name, user_login=user_login)
+	
+
+@app.route('/control/UpdateAlarmDashboard/<channel>/sensors/<sensorname>/<organization>/<dashboard_name>/<user_login>/<set_min_value>/<set_max_value>')
+def Set_Alarm(channel, sensorname, organization, dashboard_name, user_login, set_min_value, set_max_value):
+	#TBD: USER_LOGIN--> USER_EMAIL. to check in channels table the existing link between channel and user
+	# for now: just check that channel is valid, or exists
+
+	print("sensor:", sensorname) 
+	print("organization:", organization)
+	print("dashboard_name:", dashboard_name)
+	print("user_login:", user_login)
+	print("set_min_value:", set_min_value)
+	print("set_max_value:", set_max_value)
+
+
+	if sensorname == "Temperature-S":
+		pan_order = 2 #panel order in Alertes dashboard
+	elif sensorname == "AirCO2":
+		pan_order = 3
+	elif sensorname == "WaterLevel":
+		pan_order = 4
+	elif sensorname == "Oxygen":
+		pan_order = 5
+	elif sensorname == "AtmosphericTemp":
+		pan_order = 6
+	elif sensorname == "Conductivity2":
+		pan_order = 7
+	elif sensorname == "Conductivity1":
+		pan_order = 8
+	elif sensorname == "Turbidity":
+		pan_order = 9
+	elif sensorname == "pH":
+		pan_order = 10
+	elif sensorname == "Humidity":
+		pan_order = 11
+	elif sensorname == "Temperature-D":
+		pan_order = 12
+	else:
+		return "Invalid sensor name" #will appear in div
+		
+
+
+	#try:
+	
+	#	user_email = query_channels(channel)
+	#except:
+	#	return "Db Query error"
+	user_email = 1	
+
+	if user_email is not None: #so channel exists
+		try:
+			#will extract the json definition of this particular dashboard
+			#this fc already switches organization
+			print("Obtaining dashboard json")  
+			data=gr._get_dashboard_json(dashboard_name, organization) #collapsed has to be True for all rows!
+
+			data['dashboard']['panels'][pan_order]['panels'][0]['alert']['conditions'][0]['evaluator']['params'][0]= float(set_min_value)
+			data['dashboard']['panels'][pan_order]['panels'][0]['alert']['conditions'][0]['evaluator']['params'][1]= float(set_max_value)
+
+			print("Uploading dashboard json")
+
+			status, uid = gr._update_existing_dashboard(data)
+
+			#todo:
+			# html file for setting alarms
+			# upload code to github and dockerhub and mainflux docker
+			if status == "success":
+				return "OK"
+			else:
+				return "Unable to set alarm"
+		except:
+			return "Something went wrong when querying the alarm"
+	else:
+		return "Invalid channel"
+	
 
 @app.route('/calibration/Check/<target_device>/<channel>/sensors/<sensorname>')
 def cal_check(target_device, channel, sensorname):
--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import paho.mqtt.client as mqtt
 import time
 import json
 import os
+import grafana_interactions as gr
 
 app = Flask(__name__)
 CORS(app)
@@ -39,6 +40,17 @@ def query_db(publisher):
 			thing_id=row[0]
 			thing_key=row[2]
 		return thing_id,thing_key
+
+def query_channels(channel):
+	postgreSQL_select_Query = "select channels.* from channels where channels.id='"+ str(channel)+ "'"
+	cursor.execute(postgreSQL_select_Query)
+	channels_records = cursor.fetchall() 
+	user_email=None
+	if channels_records is not None:
+		for row in channels_records:
+			print("Entry = ", row, "\n")
+			user_email=row[1]
+		return user_email
 
 def on_connect(client, userdata, flags, rc):
 	if rc == 0:
@@ -128,6 +140,138 @@ def cal_sensor(db_to_use,target_device,channel,sensorname):
 			return "Connection to device failed, try again"
 	else:
 		return "Failed calibrating sensor, try again"	
+
+
+@app.route('/control/SetAlarm/<channel>/sensors/<sensorname>/<organization>/<dashboard_name>/<user_login>')
+def alarmpage(channel, sensorname, organization, dashboard_name, user_login):
+#TBD: USER_LOGIN--> USER_EMAIL. to check in channels table the existing link between channel and user
+	# for now: just check that channel is valid, or exists
+	if sensorname == "Temperature-S":
+		pan_order = 2 #panel order in Alertes dashboard
+	elif sensorname == "AirCO2":
+		pan_order = 3
+	elif sensorname == "WaterLevel":
+		pan_order = 4
+	elif sensorname == "Oxygen":
+		pan_order = 5
+	elif sensorname == "AtmosphericTemp":
+		pan_order = 6
+	elif sensorname == "Conductivity2":
+		pan_order = 7
+	elif sensorname == "Conductivity1":
+		pan_order = 8
+	elif sensorname == "Turbidity":
+		pan_order = 9
+	elif sensorname == "pH":
+		pan_order = 10
+	elif sensorname == "Humidity":
+		pan_order = 11
+	elif sensorname == "Temperature-D":
+		pan_order = 12
+	else:
+		message = "Invalid sensor name" #will appear in div
+		return render_template('alarm.html', message= message, sensor=sensorname, min_val="None", max_val = "None" , channel=channel,
+			 organization=organization, dashboard_name=dashboard_name, user_login=user_login)
+	try:
+		user_email = query_channels(channel)
+	except:
+		message = "Db Query error"
+		return render_template('alarm.html', message= message, sensor=sensorname, min_val = "None", max_val= "None", channel=channel,
+			 organization=organization, dashboard_name=dashboard_name, user_login=user_login)
+
+	if user_email is not None: #so channel exists
+		try:
+			#will extract the json definition of this particular dashboard
+			#this fc already switches organization
+			print("Obtaining dashboard json")  
+			data=gr._get_dashboard_json(dashboard_name, organization) #collapsed has to be True for all rows!
+			crt_min = data['dashboard']['panels'][pan_order]['panels'][0]['alert']['conditions'][0]['evaluator']['params'][0]
+			crt_max = data['dashboard']['panels'][pan_order]['panels'][0]['alert']['conditions'][0]['evaluator']['params'][1]
+
+			return render_template('alarm.html', message= "", sensor=sensorname, min_val=crt_min, max_val = crt_max, channel=channel,
+			 organization=organization, dashboard_name=dashboard_name, user_login=user_login)
+		except:
+			message = "Something went wrong when querying the alarm"
+			return render_template('alarm.html', message= message, sensor=sensorname, min_val="None", max_val = "None" , channel=channel,
+			 organization=organization, dashboard_name=dashboard_name, user_login=user_login)
+	else:
+		message = "Invalid channel"
+		return render_template('alarm.html', message= message, sensor=sensorname, min_val="None", max_val = "None", channel=channel,
+			 organization=organization, dashboard_name=dashboard_name, user_login=user_login)
+	
+
+@app.route('/control/UpdateAlarmDashboard/<channel>/sensors/<sensorname>/<organization>/<dashboard_name>/<user_login>/<set_min_value>/<set_max_value>')
+def Set_Alarm(channel, sensorname, organization, dashboard_name, user_login, set_min_value, set_max_value):
+	#TBD: USER_LOGIN--> USER_EMAIL. to check in channels table the existing link between channel and user
+	# for now: just check that channel is valid, or exists
+
+	print("sensor:", sensorname) 
+	print("organization:", organization)
+	print("dashboard_name:", dashboard_name)
+	print("user_login:", user_login)
+	print("set_min_value:", set_min_value)
+	print("set_max_value:", set_max_value)
+
+
+	if sensorname == "Temperature-S":
+		pan_order = 2 #panel order in Alertes dashboard
+	elif sensorname == "AirCO2":
+		pan_order = 3
+	elif sensorname == "WaterLevel":
+		pan_order = 4
+	elif sensorname == "Oxygen":
+		pan_order = 5
+	elif sensorname == "AtmosphericTemp":
+		pan_order = 6
+	elif sensorname == "Conductivity2":
+		pan_order = 7
+	elif sensorname == "Conductivity1":
+		pan_order = 8
+	elif sensorname == "Turbidity":
+		pan_order = 9
+	elif sensorname == "pH":
+		pan_order = 10
+	elif sensorname == "Humidity":
+		pan_order = 11
+	elif sensorname == "Temperature-D":
+		pan_order = 12
+	else:
+		return "Invalid sensor name" #will appear in div
+		
+
+
+	try:
+		user_email = query_channels(channel)
+	except:
+		return  "Db Query error"
+		
+
+	if user_email is not None: #so channel exists
+		try:
+			#will extract the json definition of this particular dashboard
+			#this fc already switches organization
+			print("Obtaining dashboard json")  
+			data=gr._get_dashboard_json(dashboard_name, organization) #collapsed has to be True for all rows!
+
+			data['dashboard']['panels'][pan_order]['panels'][0]['alert']['conditions'][0]['evaluator']['params'][0]= float(set_min_value)
+			data['dashboard']['panels'][pan_order]['panels'][0]['alert']['conditions'][0]['evaluator']['params'][1]= float(set_max_value)
+
+			print("Uploading dashboard json")
+
+			status, uid = gr._update_existing_dashboard(data)
+
+			#todo:
+			# html file for setting alarms
+			# upload code to github and dockerhub and mainflux docker
+			if status == "success":
+				return "OK"
+			else:
+				return "Unable to set alarm"
+		except:
+			return "Something went wrong when querying the alarm"
+	else:
+		return "Invalid channel"
+	
 
 
 @app.route('/control/calibration/Check/<target_device>/<channel>/sensors/<sensorname>')
